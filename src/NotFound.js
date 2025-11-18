f61891208a7452a30c52a01a8604f7268768e9a2
harshinibasana <75540584+harshinibasana@users.noreply.github.com>
2025-11-18 16:26:25 -0600

    Update NotFound.js

--- src/NotFound.js
+++ src/NotFound.js
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 export default function NotFound() {
   return (
     <div className="card">
-      <h3>404 — Not Found</h3>
       <Link to="/">Go home</Link>
     </div>
   );
